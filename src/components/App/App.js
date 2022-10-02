cb21c5f857edd19cfcf060975398c99627c14ada
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-10-02 09:19:11 -0500

    client side ui done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import {useState} from 'react';
+import './App.css';
 
-function App(props) {
+function App() {
+
+const [notes, setNotes] = useState('');
+const [address, setAddress] = useState('');
+const [email, setEmail] = useState({email: '', notes:''});
+
+
+const handleAddress = (event) => {
+  // setAddress(event.target.value);
+  setEmail({...email, email: event.target.value});
+  console.log(address);
+}//end of handleAddress
+
+const handleNotes = (event) => {
+  // setNotes(event.target.value);
+  setEmail({...email, notes: event.target.value })
+  console.log(notes);
+}//end of handleNotes
+
+const handleEmail = (event) => {
+  event.preventDefault();
+  console.log('clicked submit');
+  console.log(email);
+}
+
   return (
     <div>
       <h1>Email notification!</h1>
+      <form className="form" onSubmit={handleEmail}>
+        <input
+        type="text"
+        placeholder="email-address"
+        onChange={handleAddress}
+        />
+        <textarea 
+        type="text"
+        placeholder="Notes"
+        onChange={handleNotes}
+        className="notes"
+        />
+        <button
+        type="submit"
+        >
+          Send Email
+        </button>
+      </form>
     </div>
   );
 }
